9ed6ba9b478e9a9505a1a375284ce4bc4f3dd933
naraditanabila <naraditanabila@gmail.com>
2019-02-22 12:11:36 +0700

    new code

--- Uro.java
+++ Uro.java
@@ -1,47 +1,68 @@
-import java.util.*;
-import java.io.*;
-import java.math.*;
-
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+ 
+using namespace std;
+ 
 /**
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  **/
-class Player {
-
-    public static void main(String args[]) {
-        Scanner in = new Scanner(System.in);
-        int playerCount = in.nextInt(); // the amount of players (always 2)
-        int myId = in.nextInt(); // my player ID (0 or 1)
-        int zoneCount = in.nextInt(); // the amount of zones on the map
-        int linkCount = in.nextInt(); // the amount of links between all zones
+int main()
+{
+    int playerCount; // the amount of players (always 2)
+    int myId; // my player ID (0 or 1)
+    int zoneCount; // the amount of zones on the map
+    int linkCount; // the amount of links between all zones
+    cin >> playerCount >> myId >> zoneCount >> linkCount;
+    int platinumSource[zoneCount];
+    for (int i = 0; i < zoneCount; i++) {
+        int zoneId; // this zone's ID (between 0 and zoneCount-1)
+        cin >> zoneId;
+        cin >> platinumSource[zoneId];
+    }
+    
+    vector <int> adjacentZone[zoneCount];
+    for (int i = 0; i < linkCount; i++) {
+        int zone1;
+        int zone2;
+        cin >> zone1 >> zone2;
+        adjacentZone[zone1].push_back(zone2);
+        adjacentZone[zone2].push_back(zone1);
+    }
+ 
+    int ownerId[zoneCount];
+    int podsP0[zoneCount];
+    int podsP1[zoneCount];
+    bool visible[zoneCount];
+    int myPlatinum;
+    int prio[zoneCount];
+    // game loop
+    while (1) {
+        cin >> myPlatinum; 
+        //queue <int> prioQueue;
         for (int i = 0; i < zoneCount; i++) {
-            int zoneId = in.nextInt(); // this zone's ID (between 0 and zoneCount-1)
-            int platinumSource = in.nextInt(); // Because of the fog, will always be 0
+            int zId; // this zone's ID
+            cin >> zId;
+            cin >> ownerId[zId] >> podsP0[zId] >> podsP1[zId] >> visible[zId] >> platinumSource[zId]; cin.ignore();
+ 
         }
-        for (int i = 0; i < linkCount; i++) {
-            int zone1 = in.nextInt();
-            int zone2 = in.nextInt();
-        }
-
-        // game loop
-        while (true) {
-            int myPlatinum = in.nextInt(); // your available Platinum
-            for (int i = 0; i < zoneCount; i++) {
-                int zId = in.nextInt(); // this zone's ID
-                int ownerId = in.nextInt(); // the player who owns this zone (-1 otherwise)
-                int podsP0 = in.nextInt(); // player 0's PODs on this zone
-                int podsP1 = in.nextInt(); // player 1's PODs on this zone
-                int visible = in.nextInt(); // 1 if one of your units can see this tile, else 0
-                int platinum = in.nextInt(); // the amount of Platinum this zone can provide (0 if hidden by fog)
+ 
+        // Write an action using cout. DON'T FORGET THE "<< endl"
+        // To debug: cerr << "Debug messages..." << endl;
+ 
+        // first line for movement commands, second line no longer used (see the protocol in the statement for details)
+        for (int i = 0; i < zoneCount; ++i) {
+            if (((myId == 0 && podsP0[i] != 0) || (myId == 1 && podsP1[i] != 0))) {
+                int next = adjacentZone[i][0];
+                if (myId == 0) cout << podsP0[i];
+                else cout << podsP1[i];
+                cout << ' ' << i << ' ' << next << ' ';
             }
-
-            // Write an action using System.out.println()
-            // To debug: System.err.println("Debug messages...");
-
-
-            // first line for movement commands, second line no longer used (see the protocol in the statement for details)
-            System.out.println("WAIT");
-            System.out.println("WAIT");
         }
+        cout << endl;
+        cout << "WAIT" << endl;
     }
 }
